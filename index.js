7eca688a3db3c97157e38a8da3ed0efb7c235c4d
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2025-03-06 20:24:30 +0530

    third

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Click = document.getElementById('click')
 const Colourcode = document.getElementById('Value');
 const image = document.getElementById('image')
 const Timedelay = document.getElementById('Input');
+const Stopbut = document.getElementById('Stop')
 Click.addEventListener('click',()=>
 {   
 const r = Math.floor(Math.random() * 256);
@@ -17,18 +18,30 @@ console.log(`rgb(${r}, ${g}, ${b})`)
 })
 
 
-Disco.addEventListener('click', ()=>
+
+let i = true
+if(i=true)
 {
-time=Timedelay.value;
-image.src= "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDE4ZDdnZ2x3MnA4azB3Y2x3YWNhZzNmdDl1N2JiMnVyd3FxaGh2eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/z1lJAjRaB1ZwZ3kE9e/giphy.gif"   
-setInterval(()=>
-{
-const r = Math.floor(Math.random() * 256);
-const g = Math.floor(Math.random() * 256);
-const b = Math.floor(Math.random() * 256);
-document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-} ,time)
-})
+    Disco.addEventListener('click', ()=>
+        {
+        time=Timedelay.value;
+        image.src= "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDE4ZDdnZ2x3MnA4azB3Y2x3YWNhZzNmdDl1N2JiMnVyd3FxaGh2eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/z1lJAjRaB1ZwZ3kE9e/giphy.gif"   
+        clear = setInterval(()=>
+        {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        } ,time)
+        })
+
+}
+
+Stopbut.addEventListener('click',()=>
+    {
+       clearInterval(clear)
+    })
+
 })
 
 
