57a5a01ecf24925be085c8fdc3155fbfddb77d1e
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2025-03-06 19:57:23 +0530

    second

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const r = Math.floor(Math.random() * 256);
 const g = Math.floor(Math.random() * 256);
 const b = Math.floor(Math.random() * 256);
 document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-} ,2000)
+} ,time)
 })
 })
 
